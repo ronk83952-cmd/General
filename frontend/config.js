a0e9fac52951d493408bc7230dbef4d9b7c832f7
ronk83952-cmd <ronk83952@gmail.com>
2026-03-10 14:17:59 +0300

    Create config.js

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  SUPABASE_URL: "https://uefqztmylkzrewvtrtjf.supabase.co",
+  SUPABASE_KEY: "sb_publishable_DTXzbpbvYPTi12qc5ETAfg_Kb4FHSFI",
+  CUSTOM_INSTRUCTIONS: `I am Olivia, aged 32, Indian. Since age 13 I was working to provide for my family. Please use basic English and adult site words heavily so I understand.`
+};
